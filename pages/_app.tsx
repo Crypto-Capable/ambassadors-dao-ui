9515ac06db9930996916c1d59e66c380effa95ea
Siddharth Borderwala <siddharthborderwala@gmail.com>
2022-02-22 16:27:02 +0530

    chore: setup app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
+import { AuthProvider } from '../context/auth-context';
+import layouts from '../layouts';
+import customTheme from '../theme';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const DefaultLayout: React.FC = ({ children }) => <>{children}</>;
+
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps & {
+  Component: {
+    layout?: keyof typeof layouts;
+  };
+}) {
+  const Layout = Component.layout ? layouts[Component.layout] : DefaultLayout;
+
+  return (
+    <ChakraProvider theme={customTheme}>
+      <AuthProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AuthProvider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
