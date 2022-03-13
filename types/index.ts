e88d897b7bab4ba73a8b1cb4823be2ea2d7d4a3c
Siddharth Borderwala <siddharthborderwala@gmail.com>
2022-03-13 20:28:47 +0530

    create types

--- types/index.ts
+++ types/index.ts
@@ -6,22 +6,130 @@ export type LayoutPage<T> = NextPage<T> & {
   layout: Layouts;
 };
 
-export type Payout = {
-  id: string;
+export enum Tabs {
+  PROPOSALS = 'proposals',
+  BOUNTIES = 'bounties',
+  REFERRALS = 'referrals',
+  MISCELLANEOUS = 'miscellaneous',
+}
+
+export type Vote = { Approve: 0 } | { Reject: 1 };
+
+export type VotesCount = {
+  approve_count: number;
+  reject_count: number;
 };
 
-export type viewAllFnArgs = {
+export type SubmissionInfo = {
+  name: string;
+  account_id: string;
+  submission_link: string;
+};
+
+export type ProposalType =
+  | {
+      Hackathon: {
+        expected_registrations: number;
+        estimated_budget: string;
+        supporting_document: string;
+      };
+    }
+  | {
+      MemeContest: {
+        expected_registrations: number;
+        estimated_budget: string;
+        supporting_document: string;
+      };
+    }
+  | {
+      Open: {
+        estimated_budget: string;
+        supporting_document: string;
+      };
+    };
+
+export type BountyType =
+  | {
+      HackathonCompletion: {
+        num_of_registrations: number;
+        num_of_submissions: number;
+        winners_info: SubmissionInfo[];
+      };
+    }
+  | {
+      MemeContestCompletion: {
+        num_of_submissions: number;
+        winners_info: SubmissionInfo[];
+      };
+    }
+  | {
+      Webinar: {
+        num_of_registrations: number;
+        num_of_attendees: number;
+        webinar_link: string;
+      };
+    }
+  | {
+      ContentCoordination: {
+        content_links: string[];
+        story: string;
+        tools_used: string[];
+      };
+    };
+
+export type ReferralType =
+  | {
+      AmbassadorRegistration: {
+        referrer_id: string;
+        referred_id: string;
+      };
+    }
+  | 'Recruitment'
+  | 'NearCertifiedDeveloper';
+
+export type MiscellaneousType =
+  | {
+      ContentCreationBounty: {
+        links_to_content: string[];
+        expected_amount: string;
+        note: string;
+      };
+    }
+  | 'CampusSigningMOU'
+  | {
+      CampusAmbassadorBonus: {
+        links_to_payout: string[];
+      };
+    };
+
+export type PayoutStatusType =
+  | 'Approved'
+  | 'Rejected'
+  | { Removed: string | null }
+  | 'UnderConsideration';
+
+export type Payout<T> = {
+  id: number;
+  status: PayoutStatusType;
+  proposer: string;
+  info: T;
+  description: string;
+  votes: Record<string, Vote>;
+  votes_count: VotesCount;
+};
+
+export type getAllFnArgs = {
   startIndex: number;
   limit: number;
 };
 
-export type viewAllFn = (args: viewAllFnArgs) => Promise<Payout[]>;
+export type getAllFn<T> = (args: getAllFnArgs) => Promise<Payout<T>[]>;
 
 export type viewFunctionsType = {
-  viewAllProposals: viewAllFn;
-  viewAllBounties: viewAllFn;
-  viewAllReferrals: viewAllFn;
-  viewAllMiscellaneous: viewAllFn;
+  get_all_proposals: getAllFn<ProposalType>;
+  get_all_bounties: getAllFn<BountyType>;
+  get_all_referrals: getAllFn<ReferralType>;
+  get_all_miscellaneous: getAllFn<MiscellaneousType>;
 };
 
 export type changeFunctionsType = {};
