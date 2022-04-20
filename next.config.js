a90da994430785af9a6dfc708806c8ae3209a64a
Siddharth Borderwala <siddharthborderwala@gmail.com>
2022-04-20 22:28:14 +0530

    chore: configure next config for deploying on netlify

--- next.config.js
+++ next.config.js
@@ -1,17 +1,23 @@
 const fs = require('fs');
 const { withSentryConfig } = require('@sentry/nextjs');
 
-const isProduction = process.env.NODE_ENV === 'production';
-
 const env = {};
 
-if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
-  env.NEXT_PUBLIC_HOST = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+// this context corresponds to the previews we build for pull/merge requests.
+if (process.env.CONTEXT === 'deploy-preview') {
+  // DEPLOY_PRIME_URL representS the primary URL for an individual deploy, or a group of them, like branch deploys and Deploy Previews
+  env.NEXT_PUBLIC_HOST = `https://${process.env.DEPLOY_PRIME_URL}`;
 }
 
-if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'production') {
-  env.NEXT_PUBLIC_HOST = `https://${process.env.HOST}`;
+// this context corresponds to the main branch builds
+const isProductionContext = process.env.CONTEXT === 'production';
+
+if (isProductionContext) {
+  // URL corresponds to the main url of the project
+  env.NEXT_PUBLIC_HOST = `https://${process.env.URL}`;
 }
+
+// for dev env, set the NEXT_PUBLIC_HOST in the .env.development.local file itself
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -26,8 +32,13 @@ const sentryWebpackPluginOptions = {
   project: process.env.SENTRY_PROJECT_ID,
 };
 
+// if SITE_NAME is ambassadors-dao, it is production, i.e. on mainnet
+// if SITE_NAME is ambassadors-dao-dev, it is development, i.e. on testnet
+const isMainnetSite = process.env.SITE_NAME === 'ambassadors-dao';
+
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = isProduction
-  ? withSentryConfig(nextConfig, sentryWebpackPluginOptions)
-  : nextConfig;
+module.exports =
+  isProductionContext && isMainnetSite
+    ? withSentryConfig(nextConfig, sentryWebpackPluginOptions)
+    : nextConfig;
